596a2353ea2f5dec7c5a6ddcfb9e3c92e5771a3c
Ru Al <39805456+rualb@users.noreply.github.com>
2026-03-09 17:14:04 +0400

    -

--- internal/router/router.go
+++ internal/router/router.go
@@ -100,8 +100,13 @@ func initSys(e *echo.Echo, appService service.AppService) {
 
 func initDebugController(e *echo.Echo, _ service.AppService) {
 	e.GET(consts.PathProxyPingDebugAPI, func(c echo.Context) error { return c.String(http.StatusOK, "pong") })
-	// publicly-available-no-sensitive-data
-	e.GET("/health", func(c echo.Context) error { return c.JSON(http.StatusOK, struct{}{}) })
+	//
+	// status
+	e.GET("/-/health", func(c echo.Context) error { return c.JSON(http.StatusOK, struct{}{}) })
+	//
+	e.GET("/-/probe/startup", func(c echo.Context) error { return c.String(http.StatusOK, "ok") })
+	e.GET("/-/probe/ready", func(c echo.Context) error { return c.String(http.StatusOK, "ok") })
+	e.GET("/-/probe/live", func(c echo.Context) error { return c.String(http.StatusOK, "ok") })
 
 	// curl -X POST -H "Content-Type: application/json" -d '{}' http://127.0.0.1/proxy/api/status
 	// {"message":"missing csrf token in the form parameter"}
